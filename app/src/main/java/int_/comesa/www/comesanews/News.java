80c9ec746bd2befb085b23fbe57a5f137186a154
mtondolo <mtondolo@gmail.com>
2018-08-24 16:36:01 +0200

    feat: Create first layout

---
+++ app/src/main/java/int_/comesa/www/comesanews/News.java
@@ -0,0 +1,32 @@
+package int_.comesa.www.comesanews;
+
+public class News {
+
+    /**
+     * This method returns a list of new headlines.
+     *
+     * @return A list of news head lines
+     */
+    public static String[] getNews() {
+        return new String[]{
+                "COMESA Summit Timetable Released",
+                "EU Member of Parliament visits COMESA",
+                "Comoros has updated the veterinary legislation to boost livestock sector",
+                "COMESA, China seeks to deepen collaboration",
+                "Member States Urged to Speedup Seed Harmonization",
+                "Competition Commission begins probe on football deal",
+                "Global Customs Chief in COMESA to discuss partnership",
+                "Instilling mindsets and habits conducive to attaining the SDGs is critical – President Kagame",
+                "Regional Fertilizer Supply Program kicks-off",
+                "Comesa court underutilised, Senior Counsel Nzamba Kitonga says",
+                "Steps to Entrench Gender in Regional Integration",
+                "Djibouti Corridor Authority to be established",
+                "EU provides €1.3 Billion for security and integration programmes",
+                "Tripartite leadership changes hands",
+                "COMESA-EAC-SADC Tripartite launched",
+                "COMESA and Accord Deepen Collaboration",
+                "Zambia – DR Congo discuss the Simplified Trade Regime",
+                "COMESA Immigration Chiefs Meet",
+        };
+    }
+}
