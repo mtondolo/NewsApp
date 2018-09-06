d34203fab1b1c3423642dc5a14369438291cc52b
mtondolo <mtondolo@gmail.com>
2018-09-06 17:27:16 +0200

    Setup contentprovider

--- app/src/main/java/com/example/android/merchantpost/data/NewsContract.java
+++ app/src/main/java/com/example/android/merchantpost/data/NewsContract.java
@@ -15,12 +15,48 @@
  */
 package com.example.android.merchantpost.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
+/**
+ * Defines table and column names for the news database. This class is not necessary, but keeps
+ * the code organized.
+ */
 public class NewsContract {
+
+    /*
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website. A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * Play Store.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.android.merchantpost";
+
+    /*
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider for News.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /*
+     * Possible paths that can be appended to BASE_CONTENT_URI to form valid URI's that News
+     * can handle. For instance,
+     *
+     *     content://com.example.android.merchantpost/news/
+     *     [           BASE_CONTENT_URI         ][ PATH_NEWS ]
+     *
+     * is a valid path for looking at news data.
+     *
+     */
+    public static final String PATH_NEWS = "news";
 
     /* Inner class that defines the table contents of the news table */
     public static final class NewsEntry implements BaseColumns {
+
+        /* The base CONTENT_URI used to query the News table from the content provider */
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_NEWS)
+                .build();
 
         /* Used internally as the name of our news table. */
         public static final String TABLE_NAME = "news";
---
+++ app/src/main/java/com/example/android/merchantpost/data/NewsProvider.java
@@ -0,0 +1,199 @@
+package com.example.android.merchantpost.data;
+
+import android.annotation.TargetApi;
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+/**
+ * This class serves as the ContentProvider for all of News's data. This class allows us to
+ * bulkInsert data, query data, and delete data.
+ * <p>
+ * Although ContentProvider implementation requires the implementation of additional methods to
+ * perform single inserts, updates, and the ability to get the type of the data from a URI.
+ * However, here, they are not implemented for the sake of brevity and simplicity.
+ */
+public class NewsProvider extends ContentProvider {
+
+    /*
+     * These constant will be used to match URIs with the data they are looking for. We will take
+     * advantage of the UriMatcher class to make that matching MUCH easier than doing something
+     * ourselves, such as using regular expressions.
+     */
+    public static final int CODE_NEWS = 100;
+
+    /*
+     * The URI Matcher used by this content provider. The leading "s" in this variable name
+     * signifies that this UriMatcher is a static member variable of NewsProvider and is a
+     * common convention in Android programming.
+     */
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    private NewsDbHelper mOpenHelper;
+
+    /**
+     * Creates the UriMatcher that will match each URI to the CODE_NEWS constant defined above.
+     *
+     * @return A UriMatcher that correctly matches the constants for CODE_NEWS and CODE_NEWS_WITH_DATE
+     */
+    public static UriMatcher buildUriMatcher() {
+
+        /*
+         * All paths added to the UriMatcher have a corresponding code to return when a match is
+         * found. The code passed into the constructor of UriMatcher here represents the code to
+         * return for the root URI. It's common to use NO_MATCH as the code for this case.
+         */
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = NewsContract.CONTENT_AUTHORITY;
+
+        /* This URI is content://com.example.android.merchantpost/news/ */
+        matcher.addURI(authority, NewsContract.PATH_NEWS, CODE_NEWS);
+
+        return matcher;
+    }
+
+    /**
+     * In onCreate, we initialize our content provider on startup. This method is called for all
+     * registered content providers on the application main thread at application launch time.
+     * It must not perform lengthy operations, or application startup will be delayed.
+     *
+     * @return true if the provider was successfully loaded, false otherwise
+     */
+    @Override
+    public boolean onCreate() {
+
+        /*
+         * As noted in the comment above, onCreate is run on the main thread, so performing any
+         * lengthy operations will cause lag in our app. Since NewsDbHelper's constructor is
+         * very lightweight, we are safe to perform that initialization here.
+         */
+        mOpenHelper = new NewsDbHelper(getContext());
+        return true;
+    }
+
+    /**
+     * Handles query requests from clients. We will use this method in Sunshine to query for all
+     * of our weather data as well as to query for the weather on a particular day.
+     *
+     * @param uri           The URI to query
+     * @param projection    The list of columns to put into the cursor. If null, all columns are
+     *                      included.
+     * @param selection     A selection criteria to apply when filtering rows. If null, then all
+     *                      rows are included.
+     * @param selectionArgs You may include ?s in selection, which will be replaced by
+     *                      the values from selectionArgs, in order that they appear in the
+     *                      selection.
+     * @param sortOrder     How the rows in the cursor should be sorted.
+     * @return A Cursor containing the results of the query. In our implementation,
+     */
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+        Cursor cursor;
+
+        /*
+         * Here's the switch statement that, given a URI, will determine what kind of request is
+         * being made and query the database accordingly.
+         */
+        switch (sUriMatcher.match(uri)) {
+
+
+            /*
+             * When sUriMatcher's match method is called with a URI that looks EXACTLY like this
+             *
+             *      content://com.example.android.merchantpost/news/
+             *
+             * sUriMatcher's match method will return the code that indicates to us that we need
+             * to return all of the weather in our weather table.
+             *
+             * In this case, we want to return a cursor that contains every row of weather data
+             * in our weather table.
+             */
+            case CODE_NEWS: {
+                cursor = mOpenHelper.getReadableDatabase().query(
+                        NewsContract.NewsEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+
+                break;
+            }
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+
+    /**
+     * In News, we aren't going to do anything with this method. However, we are required to
+     * override it as WeatherProvider extends ContentProvider and getType is an abstract method in
+     * ContentProvider. Normally, this method handles requests for the MIME type of the data at the
+     * given URI. For example, if your app provided images at a particular URI, then you would
+     * return an image URI from this method.
+     *
+     * @param uri the URI to query.
+     * @return nothing in Sunshine, but normally a MIME type string, or null if there is no type.
+     */
+    @Override
+    public String getType(@NonNull Uri uri) {
+        throw new RuntimeException("We are not implementing getType in News.");
+    }
+
+    /**
+     * In News, we aren't going to do anything with this method. However, we are required to
+     * override it as NewsProvider extends ContentProvider and insert is an abstract method in
+     * ContentProvider. Rather than the single insert method, we are only going to implement
+     * {@link NewsProvider#bulkInsert}.
+     *
+     * @param uri    The URI of the insertion request. This must not be null.
+     * @param values A set of column_name/value pairs to add to the database.
+     *               This must not be null
+     * @return nothing in News, but normally the URI for the newly inserted item.
+     */
+    @Override
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+        throw new RuntimeException(
+                "We are not implementing insert in News. Use bulkInsert instead");
+    }
+
+    /**
+     * Deletes data at a given URI with optional arguments for more fine tuned deletions.
+     *
+     * @param uri           The full URI to query
+     * @param selection     An optional restriction to apply to rows when deleting.
+     * @param selectionArgs Used in conjunction with the selection statement
+     * @return The number of rows deleted
+     */
+    @Override
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+        throw new RuntimeException("We will implement the delete method!");
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        throw new RuntimeException("We are not implementing update in Sunshine");
+    }
+
+    /**
+     * We do not need to call this method. This is a method specifically to assist the testing
+     * framework in running smoothly. You can read more at:
+     * http://developer.android.com/reference/android/content/ContentProvider.html#shutdown()
+     */
+    @Override
+    @TargetApi(23)
+    public void shutdown() {
+        mOpenHelper.close();
+        super.shutdown();
+    }
+}
